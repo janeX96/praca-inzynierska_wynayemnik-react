decd5b2d5c5d2ea6ec9d27a3981236c6ae1098d6
s16503 <s16503@pjwstk.edu.pl>
2021-11-07 18:33:50 +0100

    Owner_Premises changed into func. comp.

--- src/pages/Owner_Premises.js
+++ src/pages/Owner_Premises.js
@@ -1,47 +1,34 @@
-import React from "react";
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import LoadData from "./LoadData";
 import keycloak from "../auth/keycloak";
 import "../styles/App.css";
 import PremisesDetails from "./PremisesDetails/PremisesDetails";
 import { Link } from "react-router-dom";
 
-class Owner_Premises extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [],
-      choosenId: -1,
-      deleteURL: "",
-      deletedMessage: "",
-      // toReload: false,
-    };
-  }
+const Owner_Premises = () => {
+  const [state, setState] = useState({
+    data: [],
+    choosenId: -1,
+    deleteURL: "",
+    deletedMessage: "",
+  });
 
-  componentDidMount() {
-    this.getData();
-  }
-
-  // componentDidUpdate() {
-  //   console.log("cdUpdate: to reload: ", this.state.toReload);
-  //   if (this.state.toReload) {
-  //     this.getData().then(() => {
-  //       this.setState({ toReload: false });
-  //     });
-  //   }
-  // }
+  //pobiernie danych
+  useEffect(() => {
+    getData();
+  }, []);
 
   // pobranie danych dot. endpointów
-  async getResources() {
+  const getResources = async () => {
     const response = await fetch("/resources.json");
     const resources = await response.json();
-
     return resources;
-  }
+  };
 
-  async getData() {
-    this.getResources().then((res) => {
-      this.setState({ deleteURL: res.urls.owner.premisesDelete });
+  const getData = async () => {
+    getResources().then((res) => {
+      setState({ ...state, deleteURL: res.urls.owner.premisesDelete });
+
       //pobranie danych z wyciągniętego adresu url
       fetch(res.urls.owner.premises, {
         headers: { Authorization: " Bearer " + keycloak.token },
@@ -63,84 +50,82 @@ class Owner_Premises extends Component {
               prem.furnished = "nie";
             }
           });
-          this.setState({ data: data });
+          setState({ ...data, data: data });
         })
         .catch((err) => {
           console.log("Error Reading data " + err);
         });
     });
-  }
+  };
 
-  findDataById(id) {
-    const res = this.state.data.find((premises) => {
+  const findDataById = (id) => {
+    const res = state.data.find((premises) => {
       return premises.premisesId === id;
     });
     // console.log("wynik: ", res);
     return res;
-  }
+  };
 
   //wybierając dany lokal zaamiętuję jego id, jeśi id jest >=0
   // to wyświetlam info, jeśli nie to pokazuje liste lokali
-  handleAction = (id) => {
+  const handleAction = (id) => {
     console.log("handleAction");
-    this.setState({
+    setState({
       choosenId: id,
     });
-    this.getData();
+    getData();
   };
 
-  deleteShowMessage = (res) => {
-    this.handleAction(-1);
+  const deleteShowMessage = (res) => {
+    handleAction(-1);
     const msg = res
       ? "Lokal został usunięty"
       : "Nie udało się usunąć lokalu...";
-    this.setState({
+    setState({
       deletedMessage: msg,
       // toReload: true,
     });
 
     setTimeout(() => {
-      this.setState({
+      setState({
         deletedMessage: "",
       });
     }, 3000);
   };
 
-  render() {
-    return (
-      <div className="content-container">
-        {this.state.choosenId >= 0 ? (
-          <PremisesDetails
-            key={this.state.choosenId}
-            action={this.handleAction}
-            deleteURL={this.state.deleteURL}
-            deleteShowMessage={(res) => this.deleteShowMessage(res)}
-            reloadData={() => this.getData()}
-            {...this.findDataById(this.state.choosenId)}
-          />
-        ) : (
-          <>
-            <h1 className="content-title">Moje lokale</h1>
-            <div>
-              <Link to="/owner-premises-new">
-                <button className="add-button" Link>
-                  Dodaj nowy
-                </button>
-              </Link>
-            </div>
-            {this.state.data.length > 0 ? (
-              <LoadData data={this.state.data} action={this.handleAction} />
-            ) : (
-              "brak"
-            )}
-          </>
-        )}
-        {this.state.deletedMessage && (
-          <h2 className="submit-message">{this.state.deletedMessage}</h2>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="content-container">
+      {state.choosenId >= 0 ? (
+        <PremisesDetails
+          key={state.choosenId}
+          action={handleAction}
+          deleteURL={state.deleteURL}
+          deleteShowMessage={(res) => deleteShowMessage(res)}
+          reloadData={() => getData()}
+          {...findDataById(state.choosenId)}
+        />
+      ) : (
+        <>
+          <h1 className="content-title">Moje lokale</h1>
+          <div>
+            <Link to="/owner-premises-new">
+              <button className="add-button" Link>
+                Dodaj nowy
+              </button>
+            </Link>
+          </div>
+          {state.data.length > 0 ? (
+            <LoadData data={state.data} action={handleAction} />
+          ) : (
+            "brak"
+          )}
+        </>
+      )}
+      {state.deletedMessage && (
+        <h2 className="submit-message">{state.deletedMessage}</h2>
+      )}
+    </div>
+  );
+};
 
 export default Owner_Premises;
