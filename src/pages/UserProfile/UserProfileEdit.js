eb7ca56bc19c3e655406c5c630a3627f13025618
mateuszsuchecki <s19130@pjwstk.edu.pl>
2021-12-20 23:28:35 +0100

    fix userEdit page

--- src/pages/UserProfile/UserProfileEdit.js
+++ src/pages/UserProfile/UserProfileEdit.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import "../../styles/App.css";
-import {text} from "react-table/src/filterTypes";
-import keycloak from "../../auth/keycloak";
+import {PUT} from "../../utilities/Request"
+import {user} from "../../resources/urls"
 
 const UserProfileEdit = (props) => {
 
@@ -25,61 +25,59 @@ const UserProfileEdit = (props) => {
         NIP_incorrect: "Niepoprawna forma NIPu"
     };
 
-    useEffect(() => {
-        getResources()
-            .then((res) => {
-                setUpdateUrl(res.urls.user)
-            })
-    }, []);
-
-    const getResources = async () => {
-        const response = await fetch("/resources.json");
-        return await response.json();
-    };
-
     const sendRequest = async () => {
         let json = JSON.stringify(userEdit);
-        const requestOptions = {
-            method: "PUT",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                Authorization: " Bearer " + keycloak.token,
-            },
-            body: json,
-        };
-
-        fetch(
-            updateUrl,
-            requestOptions
-        ).then((res) => {
-            if(res.ok) console.log("Jest OK");
-            setLoading(false);
-            return res.json();
-        }).catch((err) => {
+        PUT(user.info, json)
+            .then((res) => {
+                if (res.ok) console.log("Jest OK");
+                setLoading(false);
+                return res.json();
+            }).catch((err) => {
             setLoading(false)
             console.error("Error Reading data " + err);
-        })
+        });
     }
+
+    // const requestOptions = {
+    //     method: "PUT",
+    //     headers: {
+    //         Accept: "application/json",
+    //         "Content-Type": "application/json",
+    //         Authorization: " Bearer " + keycloak.token,
+    //     },
+    //     body: json,
+    // };
+    //
+    // fetch(
+    //     updateUrl,
+    //     requestOptions
+    // ).then((res) => {
+    //     if(res.ok) console.log("Jest OK");
+    //     setLoading(false);
+    //     return res.json();
+    // }).catch((err) => {
+    //     setLoading(false)
+    //     console.error("Error Reading data " + err);
+    // })
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(!loading){
+        if (!loading) {
             setLoading(true);
             const validation = formValidation();
 
-            if(validation.correct){
+            if (validation.correct) {
                 sendRequest()
                     .then(props.back);
-            }
-            else {
+
+            } else {
                 setErrors({
-                        firstNameError: !validation.firstName,
-                        lastNameError: !validation.lastName,
-                        emailError: !validation.email,
-                        NIPError: !validation.NIP,
-                        phoneNumberError: !validation.phoneNumber
+                    firstNameError: !validation.firstName,
+                    lastNameError: !validation.lastName,
+                    emailError: !validation.email,
+                    phoneNumberError: !validation.phoneNumber
                 });
                 setLoading(false);
             }
@@ -89,7 +87,7 @@ const UserProfileEdit = (props) => {
         const type = e.target.type;
         const name = e.target.name;
 
-        if(type === "text") {
+        if (type === "text") {
             const value = e.target.value;
             setUserEdit({...userEdit, [name]: value})
         }
@@ -99,35 +97,30 @@ const UserProfileEdit = (props) => {
         return String(email)
             .toLowerCase()
             .match(
-                /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             );
     };
 
     const formValidation = () => {
         let firstName = false;
-        if(userEdit.firstName.length > 0 && userEdit.firstName.length < 31) firstName = true;
+        if (userEdit.firstName.length > 0 && userEdit.firstName.length < 31) firstName = true;
 
         let lastName = false;
-        if(userEdit.lastName.length > 0 && userEdit.lastName.length < 31) lastName = true;
+        if (userEdit.lastName.length > 0 && userEdit.lastName.length < 31) lastName = true;
 
         let phoneNumber = false;
-        if(userEdit.phoneNumber.length === 9) phoneNumber = true;
-
-        let NIP = false;
-        if(userEdit.NIP.length === 0 ||
-            (userEdit.NIP.match(/^[0-9]{10}$/) && userEdit.NIP.length === 10)) NIP = true;
+        if (userEdit.phoneNumber.length === 9) phoneNumber = true;
 
         let email = false;
-        if(userEdit.email.length < 61) email = true;
+        if (validateEmail(userEdit.email) && userEdit.email.length < 61) email = true;
 
-        let correct = firstName && lastName && phoneNumber && NIP && email;
+        let correct = firstName && lastName && phoneNumber && email;
 
         return {
             correct,
             firstName,
             lastName,
             phoneNumber,
-            NIP,
             email
         };
     }
@@ -216,12 +209,13 @@ const UserProfileEdit = (props) => {
     return (
         <div className="form-container">
             <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-25">
+                <div className="form-container__row">
+                    <div className="row__col-25">
                         <label htmlFor="firstName">Imie</label>
                     </div>
-                    <div className="col-75">
+                    <div className="row__col-75">
                         <input
+                            className="form-container__input"
                             type="text"
                             name="firstName"
                             id="firstName"
@@ -233,12 +227,13 @@ const UserProfileEdit = (props) => {
                         )}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-25">
+                <div className="form-container__row">
+                    <div className="row__col-25">
                         <label htmlFor="lastName">Nazwisko</label>
                     </div>
-                    <div className="col-75">
+                    <div className="row__col-75">
                         <input
+                            className="form-container__input"
                             type="text"
                             name="lastName"
                             id="lastName"
@@ -250,12 +245,13 @@ const UserProfileEdit = (props) => {
                         )}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-25">
+                <div className="form-container__row">
+                    <div className="row__col-25">
                         <label htmlFor="email">Email</label>
                     </div>
-                    <div className="col-75">
+                    <div className="row__col-75">
                         <input
+                            className="form-container__input"
                             type="text"
                             name="email"
                             id="email"
@@ -267,29 +263,13 @@ const UserProfileEdit = (props) => {
                         )}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-25">
-                        <label htmlFor="NIP">NIP</label>
-                    </div>
-                    <div className="col-75">
-                        <input
-                            type="text"
-                            name="NIP"
-                            id="NIP"
-                            value={userEdit.NIP}
-                            onChange={handleChange}
-                        />
-                        {errors.NIPError && (
-                            <span className="error-msg">{messages.NIP_incorrect}</span>
-                        )}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-25">
+                <div className="form-container__row">
+                    <div className="row__col-25">
                         <label htmlFor="phoneNumber">Numer telefonu</label>
                     </div>
-                    <div className="col-75">
+                    <div className="row__col-75">
                         <input
+                            className="form-container__input"
                             type="text"
                             name="phoneNumber"
                             id="phoneNumber"
@@ -301,9 +281,12 @@ const UserProfileEdit = (props) => {
                         )}
                     </div>
                 </div>
-                <button type="submit">Zapisz</button>
+                <div className="form-container__buttons">
+                    <button onClick={props.back}>Cofnij</button>
+                    <button type="submit">Zapisz</button>
+                </div>
             </form>
-            <button onClick={props.back}>Cofnij</button>
+
         </div>
     );
 };
