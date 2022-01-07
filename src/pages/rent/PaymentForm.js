252b31c3537ece09da903d4034b080cb89d5b4a7
s16503 <s16503@pjwstk.edu.pl>
2022-01-07 17:48:50 +0100

    button disabled style changed

--- src/pages/rent/PaymentForm.js
+++ src/pages/rent/PaymentForm.js
@@ -32,68 +32,84 @@ const PaymentForm = (props) => {
 
   return (
     <div className="form-container--table">
-      <div className="form-container--table__column-titles">
-        <div className="column-title">Nazwa</div>
-        <div className="column-title">Ilość</div>
-        <div className="column-title">Jedn.</div>
-        <div className="column-title">Cena netto</div>
-        <div className="column-title">VAT%</div>
-        <div className="column-title">Wart. netto</div>
-        <div className="column-title">Wart. brutto</div>
-      </div>
+      {media !== undefined && media.length > 0 ? (
+        <>
+          {" "}
+          <div className="form-container--table__column-titles">
+            <div className="column-title">Nazwa</div>
+            <div className="column-title">Ilość</div>
+            <div className="column-title">Jedn.</div>
+            <div className="column-title">Cena netto</div>
+            <div className="column-title">VAT%</div>
+            <div className="column-title">Wart. netto</div>
+            <div className="column-title">Wart. brutto</div>
+          </div>
+          <form onSubmit={handleSubmit} className="form-container--table__form">
+            {media.map((m) => (
+              <>
+                <input
+                  type="text"
+                  id="productName"
+                  name="productName"
+                  className="form-container--table__input--250"
+                  value={m.product.productName}
+                  //   onChange={}
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                  value={m.product.productName}
+                  //   onChange={}
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                />
+                <input
+                  type="text"
+                  id="1"
+                  name="1"
+                  className="form-container--table__input"
+                />
+              </>
+            ))}
+          </form>
+        </>
+      ) : (
+        "Brak mediów"
+      )}
 
-      <form onSubmit={handleSubmit} className="form-container--table__form">
-        {media.map((m) => (
-          <>
-            <input
-              type="text"
-              id="productName"
-              name="productName"
-              className="form-container--table__input--250"
-              value={m.product.productName}
-              //   onChange={}
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-              value={m.product.productName}
-              //   onChange={}
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-            />
-            <input
-              type="text"
-              id="1"
-              name="1"
-              className="form-container--table__input"
-            />
-          </>
-        ))}
-      </form>
+      <div className="form-container__buttons">
+        <button onClick={props.return}>Powrót</button>
+        <button
+          type="submit"
+          disabled={media === undefined || media.length === 0}
+        >
+          Zapisz
+        </button>
+      </div>
     </div>
   );
 };
