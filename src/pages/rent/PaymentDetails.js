d9a7bb5d8429d88327ea1a2a9fb0fb0d159aa461
s16503 <s16503@pjwstk.edu.pl>
2022-01-16 18:15:17 +0100

    display positions on paymen, todo: tabulator not works

--- src/pages/rent/PaymentDetails.js
+++ src/pages/rent/PaymentDetails.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { admin, general, owner } from "../../resources/urls";
 import { GET } from "../../utilities/Request";
+import "react-tabulator/lib/styles.css";
+import "react-tabulator/lib/css/tabulator.min.css";
+import { ReactTabulator as Tabulator } from "react-tabulator";
 
 const PaymentDetails = (props) => {
   const [payment, setPayment] = useState();
@@ -24,16 +27,123 @@ const PaymentDetails = (props) => {
     getData();
   }, []);
 
+  const columns = [
+    {
+      title: "Id",
+      field: "positionOnPaymentId",
+      visible: false,
+    },
+    {
+      title: "Nazwa",
+      field: "name",
+    },
+    {
+      title: "Cena",
+      field: "mediaRent.price",
+    },
+    {
+      title: "Ilość",
+      field: "mediaRent.quantity",
+    },
+    {
+      title: "Vat",
+      field: "mediaRent.vat",
+    },
+    {
+      title: "Wart. brutto",
+      field: "mediaRent.bruttoPrice",
+    },
+  ];
+
+  const renderTable = (data) => {
+    return (
+      <Tabulator
+        columns={columns}
+        data={data}
+        options={{
+          movableColumns: true,
+          movableRows: true,
+          pagination: true,
+          paginationSize: 5,
+          setFilter: true,
+        }}
+        layout="fitColumns"
+        responsiveLayout="hide"
+        tooltips="true"
+        addRowPos="top"
+        history="true"
+        movableColumns="true"
+        resizableRows="true"
+        initialSort={[
+          //set the initial sort order of the data
+          { column: "name", dir: "asc" },
+        ]}
+      />
+    );
+  };
+
+  const renderData = () => {
+    return (
+      <>
+        <ul>
+          <li>
+            Numer płatności:
+            <b>{payment.numberPayment}</b>
+          </li>
+          <li>
+            Status:
+            <b>{payment.status}</b>
+          </li>
+          <li>
+            Rodzaj:
+            <b>{payment.paymentType.name}</b>
+          </li>
+          <li>
+            Data wystawienia:
+            <b>{payment.startDate}</b>
+          </li>
+          <li>
+            Termin płatności:
+            <b>{payment.paymentDate}</b>
+          </li>
+          <li>
+            Zapłacono:
+            <b>{payment.paidDate !== null ? payment.paidDate : "---"}</b>
+          </li>
+          <li>
+            Przychodząca:
+            <b>{payment.income ? "tak" : "nie"}</b>
+          </li>
+          {/* <li>
+            <div className="table-container">
+              Pozycje:
+              {payment.positionOnPaymentSet.map((position) => (
+                <ul>
+                  <li>{position.name}</li>
+
+                
+                </ul>
+              ))}
+            </div>
+          </li> */}
+        </ul>
+        <b>Pozycje:</b>
+        {payment.positionOnPaymentSet.map((position) => (
+          <ul>
+            <li>{position.name}</li>
+
+            {/* {renderTable(position)} todo*/}
+          </ul>
+        ))}
+      </>
+    );
+  };
+
   return (
     <>
       <h1 className="content-container__title">Szczegóły płatności</h1>
       <div className="details-container">
-        <ul>
-          <li>
-            Lokal:
-            <b></b>
-          </li>
-        </ul>
+        {payment !== undefined && renderData()}
       </div>
     </>
   );
