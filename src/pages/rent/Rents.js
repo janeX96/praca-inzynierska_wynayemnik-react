32a56cfe883df53f2077232060346e546010d952
s16503 <s16503@pjwstk.edu.pl>
2022-01-16 16:14:43 +0100

    rent last month payment state in tabulator

--- src/pages/rent/Rents.js
+++ src/pages/rent/Rents.js
@@ -55,6 +55,17 @@ const Rents = (props) => {
 
     return `<button>Szczegóły</button>`;
   };
+
+  var customMutator = function (value, data, type, params, component) {
+    //value - original value of the cell
+    //data - the data for the row
+    //type - the type of mutation occurring  (data|edit)
+    //params - the mutatorParams object from the column definition
+    //component - when the "type" argument is "edit", this contains the cell component for the edited cell, otherwise it is the column component for the column
+
+    return value ? "opłacony" : "nieopłacony"; //return the new value for the cell data.
+  };
+
   const columns = [
     {
       title: "Id",
@@ -95,6 +106,15 @@ const Rents = (props) => {
     {
       title: "Koniec",
       field: "endDate",
+    },
+    {
+      title: "Płatności",
+      field: "paymentThisMonth",
+      mutator: customMutator,
+      cssClass:
+        customMutator === "opłacony"
+          ? "details-container__field-avb"
+          : "details-container__field-hired",
     },
     {
       formatter: actionButton,
